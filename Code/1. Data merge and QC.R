1410584c40b3984905fa325afa8cf5772941df7f
Corinne Amir <Corinne.Amir@nmfs.local>
2024-02-28 10:08:03 -1000

    Adding Sam's data
    Sam's data added to dataset. Changes based on getting new laptop too.

--- Code/1. Data merge and QC.R
+++ Code/1. Data merge and QC.R
@@ -3,12 +3,13 @@
 # Sept 2023
 
 library(tidyverse)
+library(dplyr)
 library(reshape2)
 
 
 # Import 2020-2022 data
 # setwd("E:/csv exports/") # External hard drive with 2020-2022 raw data
-setwd("C:/Users/Corinne.Amir/Documents/GitHub/Cmorph_Removal/CSV files/VPI exports/")
+setwd("C:/Users/Corinne.Amir/Documents/GitHub/Cmorph_Removal/CSV files/2020-2022 VPI exports/")
 
 filenames = list.files(pattern="\\.csv$")
 raw <- do.call("rbind", sapply(filenames, read.csv, simplify = FALSE))
@@ -22,6 +23,35 @@ raw$year <- str_sub(raw$site,5,8)
 raw$site <- str_sub(raw$site,13,15)
 
 raw[is.na(raw)] <- 0
+
+
+
+
+# Import json files (2) sent in from Sam and merge with raw
+setwd("C:/Users/Corinne.Amir/Documents/GitHub/Cmorph_Removal/CSV files/Sam exports/")
+
+filenames = list.files(pattern="\\.csv$")
+sam <- do.call("rbind", sapply(filenames, read.csv, simplify = FALSE))
+
+sam<- tibble::rownames_to_column(sam, "row_names")
+
+sam$year <- str_sub(sam$row_names,5,8)
+sam$site <- str_sub(sam$row_names,13,15)
+
+sam <- sam %>% group_by(site) %>% 
+  mutate(percent = 100*(count/sum(count))) %>%
+  select(-row_names)
+
+sam <- left_join(sam, distinct(raw[,c("id","class")]))
+
+sam[is.na(sam)] <- 0
+
+col_order <- colnames(raw)
+sam <- sam[,col_order]
+
+a <- rbind(raw, sam)
+
+
 
 
 
@@ -50,6 +80,7 @@ colnames(raw19) <- c("site","class","count","percent","year")
 
 
 # Import 2014-2018 data and format to merge with 2019-2022 dataset
+setwd("C:/Users/Corinne.Amir/Documents/GitHub/Cmorph_Removal/CSV files/")
 raw14 <- read.csv("1408-1809_Point Count_RAW.csv")
 
 raw14 <- select(raw14,-Grand.Total)
@@ -64,8 +95,6 @@ colnames(raw14) <- c("site","year","TRT","class","count")
 raw14$site <- as.character(raw14$site)
 raw14$year <- as.character(raw14$year)
 raw14$class <- as.character(raw14$class)
-
-# Merge 2014-2018, 2019, and 2020-2022 datasets
 
 raw14 <- raw14 %>% mutate(class = replace(class, class == "A..acuminata", "A. acuminata"))
 raw14 <- raw14 %>% mutate(class = replace(class, class == "M..capitata", "M. capitata"))
@@ -86,6 +115,9 @@ merge <- full_join(raw, raw19)
 a <- merge %>% group_by(site,year,class) %>% 
              summarise(totcount = sum(count)) %>%
              filter(totcount > 0)
+
+b <- merge %>% group_by(year) %>% 
+  summarise(totcount = n_distinct(site))
 
   # Remove classes that don't show up throughout entire dataset
 merge <- merge %>% filter(class != "Unk_soft_coral" & class != "Review" & class != "Brillo spike pad"
@@ -110,7 +142,12 @@ merge <- merge %>% mutate(class = replace(class, class == "Pavona spp","Pavona s
 merge <- merge %>% mutate(class = replace(class, class == "Porites superfusa","P. superfusa"))
 
 
+
 # Merge 2014-2018 with 2019-2022 dataset
 
-# merge1 <- full_join(raw14,merge)
-# merge1 <- select(merge1,-id)
+merge1 <- full_join(raw14,merge)
+merge1 <- select(merge1,-id)
+
+# Take a look at the data
+b <- merge1 %>% group_by(year) %>% 
+  summarise(totcount = n_distinct(site))
